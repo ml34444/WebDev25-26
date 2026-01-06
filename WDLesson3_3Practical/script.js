eeab4b2c26b480ddb67a604d53a53498775f32b8
ml34444 <mattl4@nycstudents.net>
2026-01-06 18:14:45 +0000

    dfgvxzvbcvxb

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,17 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+
+function css(){
+    document.getElementById("highlight").innerHTML="Cascade Style Sheets"
+}
+function js(){
+    document.getElementById("hightlight").innerHTML="Javascript"
+}
+function html(){
+    document.getElementById("hightlight").innerHTML="Hypertext Markup Language"
+}
+function all(){
+    document.getElementById("hightlight").innerHTML="CSS, Javascript, and HTML"
+}
+function web(){
+    document.getElementById("hightlight").innerHTML="Webicons"
+}
