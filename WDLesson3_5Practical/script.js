6451ec60edb22efab9dfd79b3781bfd0ed7f822f
ml34444 <mattl4@nycstudents.net>
2026-01-22 17:46:55 +0000

    hlkjbghjhjgiuyjkjk

--- WDLesson3_5Practical/script.js
+++ WDLesson3_5Practical/script.js
@@ -4,11 +4,20 @@
       3) Display the results in the appropriate element
 */
 function recArea(){
+      let rlen = parseFloat(document.getElementById("rl").value);
+      let rwid = parseFloat(document.getElementById("rw").value);
+      let op = document.getElementById("output");
+      let rA = rlen*rwid;
+      raoutput.innerHTML = rA;
 
 }
 
-function recPerimeter(){
-
+function recPer(){
+      let rlen = parseFloat(document.getElementById("rl").value);
+      let rwid = parseFloat(document.getElementById("rw").value);
+      let op = document.getElementById("output");
+      let rP = 2*(rlen + rwid);
+      rpoutput.innerHTML = rP;
 }
 
 
@@ -20,11 +29,17 @@ function recPerimeter(){
 let pi = 3.1415926;
 
 function cirArea(){
-
+      let crad = parseFloat(document.getElementById("cr").value);
+      let op = document.getElementById("output");
+      let cA = pi * (crad ** 2);
+      op.innerHTML = cA;
 }
 
 function cirPerimeter(){
-
+      let crad = parseFloat( document.getElementById("cr").value);
+      let op = document.getElementById("output");
+      let cper = 2*pi*crad;
+      op.innerHTML = cper;
 }
 
 /* Challenge Bonus: Complete the area and perimeter functions below for the triangle.  Guidelines,
--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -1,7 +1,32 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
 
+function GradeAverage(){
+    let Gr1 = parseFloat(document.getElementById("G1").value);
+    let Gr2 = parseFloat(document.getElementById("G2").value);
+    let Gr3 = parseFloat(document.getElementById("G3").value);
+    let op = document.getElementById("output");
+    let avg = (Gr1 + Gr2 + Gr3) / 3;
+    op.innerHTML = `The average of the three grades in ${avg}`;
+}
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
 
+function Slope(){
+    let y2 = parseFloat(document.getElementById("Y2").value);
+    let y1 = parseFloat(document.getElementById("Y1").value);
+    let x2 = parseFloat(document.getElementById("X2").value);
+    let x1 = parseFloat(document.getElementById("X1").value);
+    let Soutput = document.getElementById("Slope");
+    let s = (y2 - y1) / (x2 - x1);
+    Soutput.innerHTML=`The slope is ${s}`;
+}
 
 // Challenge 6:  Create the function for the BMI program.  Refer to the resource link in the HTML for assistance.
+
+function BMI(){
+    let W = parseFloat(document.getElementById("Height").value);
+    let H = parseFloat(document.getElementById("Weight").value);
+    let Boutput = document.getElementById("Boutput");
+    let BMI = (703 * W) / (H ** 2);
+    Boutput.innerHTML=`The BMI is ${BMI}`;
+}
